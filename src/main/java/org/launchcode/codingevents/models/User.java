ca8f84834b2e87824b011b4333ef12bdda4ca6bd
wisevaishu <vpm.win@gmail.com>
2024-06-10 16:39:09 -0500

    chapter 19 authentication chapter exercise

--- src/main/java/org/launchcode/codingevents/models/User.java
+++ src/main/java/org/launchcode/codingevents/models/User.java
@@ -1,8 +1,35 @@
 package org.launchcode.codingevents.models;
 
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Entity
 public class User extends AbstractEntity{
+    @NotNull
+    private String username;
 
+    @NotNull
+    private String pwHash;
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        //this.pwHash = password;
+        this.pwHash = encoder.encode(password);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
+
+//        String candidateHash = encoder.encode(password);
+//        return candidateHash.equals(pwHash);
+    }
 }
